5f9446c434bd1e42878c5a7d02e7cb1267412c80
rionpy <rionpy@gmail.com>
2016-11-28 20:02:35 +0200

    Build front-end

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,3 +1,11 @@
+
+wageApp.directive( 'csvInput', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'js/directives/csvInput.ejs',
+	}
+} );
+
 /**
  * Implements the csv-reader attribute for elements.
  * Reads a file selected with the input element.
@@ -9,7 +17,9 @@ wageApp.directive( 'csvReader', function() {
 			element.on( 'change', function( fileSelectEvent ) {
 				var files = fileSelectEvent.target.files;
 				if ( files.length ) {
-					console.log(files[0]);
+					scope.$apply( function() {
+						scope.csvFilename = files[0].name;
+					} );
 					// Use FileReader() from File API
 					var reader = new FileReader();
 					reader.onload = function( readerEvent ) {
@@ -30,5 +40,25 @@ wageApp.directive( 'wageList', function() {
 	return {
 		restrict: 'E',
 		templateUrl: 'js/directives/wageList.ejs',
+	}
+} );
+
+wageApp.directive( 'bindHeightTo', function( $window ) {
+	return {
+		restrict: 'A',
+		link: function( scope, element, attrs ){
+			scope.$watch( function() { return element[0].clientHeight }, function( newVal, oldVal ) {
+				scope[ attrs.bindHeightTo ] = newVal;
+			});
+			function updateHeight() {
+				scope[ attrs.bindHeightTo ] = element[0].clientHeight;
+				scope.$apply();
+			}
+			element.on( 'load', function() {
+				scope[ attrs.bindHeightTo ] = element[0].clientHeight;
+				scope.$apply();
+			});
+			angular.element( $window ).on( 'resize', updateHeight );
+		}
 	}
 } );
